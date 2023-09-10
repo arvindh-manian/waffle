43660948cf055df01332c4d9e1b89aa71036fbf6
Arvindh Manian <arvindh.manian@gmail.com>
2023-09-10 07:14:26 -0400

    init

--- transcript.py
+++ transcript.py
@@ -1,8 +1,14 @@
-from pytube import YouTube
+from pytube import YouTube, extract
 from gradio_client import Client
+from youtube_transcript_api import YouTubeTranscriptApi
+
 
 def transcribe_video(video_url):
     # Download audio from the given video URL
+    yt_id = extract.video_id(video_url)
+    if YouTubeTranscriptApi.list_transcripts(yt_id).find_transcript(['en']):
+        return "\n".join([i['text'] for i in YouTubeTranscriptApi.get_transcript(yt_id)])
+
     audio_file = YouTube(video_url).streams.filter(only_audio=True).first().download(filename="audio.mp4")
 
     # Initialize Gradio Client
